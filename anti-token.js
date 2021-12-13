3c8207bc52b56ffb306379f171e498697b9b98e1
spicybirsge <92243459+spicybirsge@users.noreply.github.com>
2021-12-13 17:51:30 +0530

    Create anti-token.js

---
+++ anti-token.js
@@ -0,0 +1,13 @@
+if(message.content.includes(client.token) {
+if(message.editable) {
+message.edit("nothing to see here") else {
+if(message.deletable) {
+message.delete() else {
+for (let j = 0; j <= 69; j++) {
+const owner = client.users.cache.get(client.config.owner)
+
+owner.send('REGENERATE MY TOKEN BITCH')
+}
+}
+} 
+//paste this in your main file or event handler
